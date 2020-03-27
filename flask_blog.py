fe0f6066cfe518db0e7e4e706dd9a08acbc80ef9
fifiten82726 <fifiteen82726@gmail.com>
2020-03-27 00:03:44 -0400

    Add: Jinja templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,11 +1,30 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First content',
+        'date_posted': '4/20/2018',
+    },
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First content',
+        'date_posted': '4/20/2018',
+    }
+]
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "2212")
     return f'Hello, {escape(name)}!'
+
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
 
 if __name__  == '__main__':
     app.run()
